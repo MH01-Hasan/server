426cba723068f4f272f06afebc8d954a918039cc
Mehedi hasan <mehediph2021@gmail.com>
2022-05-17 20:35:06 +0600

    done

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
-const express = require("express");
-const { MongoClient } = require("mongodb");
-const cors = require("cors");
-const app = express();
+const express = require('express')
+const app = express()
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
-// middleware
-app.use(cors());
+const cors = require('cors')
+const { MongoClient } = require('mongodb');
 
+
+const ObjectId = require('mongodb').ObjectId
+
+
+
+//middle ware//
+app.use(cors());
+app.use(express.json());
+//middle ware//
+
+
+
+
+//NEW CONNECT to data base and node server code end//
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ruek4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//NEW CONNECT to data base and node server code end//
 
 async function run() {
   try {
     await client.connect();
-    // console.log("Database Connected");
-  } finally {
+    const database = client.db("E-commerce");
+
+    //*******************************all get api************************** */
+  }
+
+  finally {
     // await client.close();
   }
+
 }
+
 run().catch(console.dir);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 });
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log("Example", port)
 });
